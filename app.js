3d8a8cdf68af3a214b04bb216b869824f58e087a
Jamie Collinson <jamiecollinson@gmail.com>
2013-09-29 21:16:57 +0000

    add zoom behaviour and improve continent colouring

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ function initialize() {
   areas = [];
   
   mapOptions = {
-    center: new google.maps.LatLng(0, 0),
+    center: new google.maps.LatLng(31.50362930577303, 14.4140625),
     zoom: 2,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     streetViewControl: false
   };
   countryInitOptions = {
     fillColor: '008FCC',
-    fillOpacity: 0.2,
+    fillOpacity: 0.5,
     strokeOpacity: 0
   };
   countryHoverOptions = {
+    fillColor: 'EC7404',
     fillOpacity: 0.5,
     strokeOpacity: 0
+  };
+  countrySelectOptions = {
+    fillColor: 'EC7404',
+    fillOpacity: 0.8,
+    strokeOpacity: 1
   };
   
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -54,8 +60,8 @@ function addAreaListeners() {
   for (var i=0; i<areas.length; i++){
     google.maps.event.addListener(areas[i], 'mouseover', function() {
       for (var j=0; j<areas.length; j++){
-        if (areas[j].geojsonProperties.id == this.geojsonProperties.id) {
-          areas[j].setOptions( {fillOpacity: 0.8, strokeOpacity: 0.5} );
+        if (areas[j].geojsonProperties.id == this.geojsonProperties.id && map.getZoom()>2) {
+          areas[j].setOptions(countrySelectOptions);
         } else if (areas[j].geojsonProperties.continent == this.geojsonProperties.continent) {
           areas[j].setOptions(countryHoverOptions);
         }
@@ -64,6 +70,16 @@ function addAreaListeners() {
     google.maps.event.addListener(areas[i], 'mouseout', function() {
       for (var j=0; j<areas.length; j++){
         areas[j].setOptions(countryInitOptions);
+      }
+    });
+    google.maps.event.addListener(areas[i], 'click', function(event) {
+      if (map.getZoom() <= 2) {
+        if (this.geojsonProperties.continent == 'middle east'){
+          map.setZoom(5);
+        } else {
+          map.setZoom(3);
+        }
+        map.setCenter(event.latLng);
       }
     });
   }
