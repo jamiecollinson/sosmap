64ede6dcde945f259d1ba1354df2f9fa7cf86c35
Jamie Collinson <jamiecollinson@gmail.com>
2013-10-06 16:12:24 +0000

    refactor village and country table IDs into vars, add test of query chande on country click

--- fusion-tables/app.js
+++ fusion-tables/app.js
@@ -1,7 +1,8 @@
 function initialize() {  
-  
-  google.maps.visualRefresh = true;
-  
+
+  villageTable = '1_n1A_kTBRcXm_YcIpxV53zs4kj7kedMfioDOtkY';
+  countryTable = '1y7kgNaV_rgIgFeWwcY6jCIKPPRrR-GJvXicf7sw';
+    
   mapOptions = {
     center: new google.maps.LatLng(31.50362930577303, 14.4140625),
     zoom: 2,
@@ -10,16 +11,18 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     streetViewControl: false
   };
+  google.maps.visualRefresh = true;
   
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   
+  // initialise info window
   var infoWindow = new google.maps.InfoWindow();
   
   // initialise country layer
   var countryLayer = new google.maps.FusionTablesLayer({
     query: {
       select: 'kml',
-      from: '1y7kgNaV_rgIgFeWwcY6jCIKPPRrR-GJvXicf7sw'
+      from: countryTable
     },
     styles: [{
       polygonOptions: {
@@ -40,25 +43,42 @@ function initialize() {
     suppressInfoWindows: true
   });
   
-  // add event listener for country layer
-  google.maps.event.addListener(countryLayer, 'click', function(e) {
-    windowControl(e, infoWindow, map);
-  });
-  
   // initialise village layer
   var villageLayer = new google.maps.FusionTablesLayer({
     query: {
       select: 'kml',
-      from: '1_n1A_kTBRcXm_YcIpxV53zs4kj7kedMfioDOtkY'
+      from: villageTable
     },
     styleId: 2,
     map: map,
     suppressInfoWindows: true
   });
 
+  // add event listener for country layer
+  google.maps.event.addListener(countryLayer, 'click', function(e) {
+    windowControl(e, infoWindow, map);
+    villageLayer.setOptions({
+      query: {
+        select: 'kml',
+        from: villageTable,
+        where: "iso_a2 LIKE '" + e.row['iso_a2'].value + "'"
+      }
+    });
+  });
+  
   // add event listener for village layer
   google.maps.event.addListener(villageLayer, 'click', function(e) {
     windowControl(e, infoWindow, map);
+  });
+  
+  // add event listener for info window
+  google.maps.event.addListener(infoWindow, 'closeclick', function(e) {
+    villageLayer.setOptions({
+      query: {
+        select: 'kml',
+        from: villageTable
+      }
+    });
   });
   
 }
