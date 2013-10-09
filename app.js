fe4c1056d4eba08d2355e48eee396e8f21c05a12
Jamie Collinson <jamiecollinson@gmail.com>
2013-10-09 14:07:41 +0000

    load villages from fusion table as markers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function initialize() {
 
   villageTable = '1_n1A_kTBRcXm_YcIpxV53zs4kj7kedMfioDOtkY';
   countryTable = '1y7kgNaV_rgIgFeWwcY6jCIKPPRrR-GJvXicf7sw';
+  googleBrowserKey = 'AIzaSyD2NNOm9P6iW0Kw8NHb1SwcMwD4YB1fUiw';
     
   mapOptions = {
     center: new google.maps.LatLng(31.50362930577303, 14.4140625),
@@ -14,7 +15,7 @@ function initialize() {
   
   google.maps.visualRefresh = true;
   
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   
   // add panel control
   panelControl = new panelControl(map);
@@ -43,22 +44,71 @@ function initialize() {
     suppressInfoWindows: true
   });
   
-  // initialise village layer
-
+  // initialise village layer & add to window so villages.callback is available
+  var villages = new villageControl(villageTable, googleBrowserKey);
+  window.villages = villages;
+  
   // add event listener for country layer
   google.maps.event.addListener(countryLayer, 'click', function(e) {
-    countryControl(countryLayer, e);
+    countryControl(countryLayer, e, villages, map);
   });
   
 }
 
 // controller for villages layer
-function villageControl() {
+function villageControl(villageTable, fusionTablesAPIKey) {
+  // jsonp query to get village data from fusion tables
+  var script = document.createElement('script');
+  var url = ['https://www.googleapis.com/fusiontables/v1/query?'];
+  url.push('sql=');
+  var query = 'SELECT name, lat, lng, iso_a2 FROM ' +
+    villageTable;
+  var encodedQuery = encodeURIComponent(query);
+  url.push(encodedQuery);
+  url.push('&callback=villages.callBack');
+  url.push('&key=' + googleBrowserKey);
+  script.src = url.join('');
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(script);
   
+  this.villages = [];
+  
+  this.addAllToMap = function(map) {
+    for (var i=0; i<this.villages.length; i++) {
+      this.villages[i].setMap(map);
+    }
+  }
+  
+  this.addToMap = function(map, iso_a2) {
+    var villages = this.villages;
+    for (var i=0; i<villages.length; i++) {
+      if (villages[i].iso_a2 === iso_a2) {
+        villages[i].setMap(map);
+      } else {
+        villages[i].setMap();
+      }
+    }
+  }
+  
+  this.callBack = function(data) {
+    var rows = data['rows'];
+    for (var i=0; i<rows.length; i++) {
+      var icon = 'img/bg-marker-2.png';
+      var latLng = new google.maps.LatLng(rows[i][1],rows[i][2]);
+      var marker = new google.maps.Marker({
+        position: latLng,
+        title: rows[i][0],
+        iso_a2: rows[i][3],
+        icon: icon
+      });
+      this.villages.push(marker);
+    }
+  }
 }
 
 // controller for country layer
-function countryControl(countryLayer, e) {
+function countryControl(countryLayer, e, villages, map) {
+  var iso_a2 = e.row['iso_a2'].value;
   panelControl.update(e);
   countryLayer.setOptions({
     styles: [{
@@ -72,17 +122,18 @@ function countryControl(countryLayer, e) {
         fillColor: "#009AE0"
       }
     },{
-      where: "iso_a2 LIKE '" + e.row['iso_a2'].value + "'",
+      where: "iso_a2 LIKE '" + iso_a2 + "'",
       polygonOptions: {
         fillOpacity: 0.01
       }
     }]
   });
+  villages.addToMap(map, iso_a2);
 }
 
 // add custom control for country info panel
 function panelControl(map) {
-  div = document.createElement('div');
+  var div = document.createElement('div');
   div.style.margin = '5px';
   div.style.backgroundColor = 'white';
   div.innerHTML = '<h2>Choose a country</h2>';
@@ -96,7 +147,7 @@ function panelControl(map) {
 
 // open the info window at the clicked location
 function windowControl(e, infoWindow, map) {
-  content = 'This will contain info about the facility';
+  var content = 'This will contain info about the facility';
   infoWindow.setOptions({
     content: content,
     position: e.latLng,
